4931f2eadb5f8541fe621e49711dc8332b2e9b02
Gerold Penz <gerold@halvar.at>
2018-04-15 17:50:01 +0200

    Prod

--- application/my_flask_app.py
+++ application/my_flask_app.py
@@ -6,24 +6,22 @@ import sys
 import flask
 import redis
 
-
 THISDIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, THISDIR)
-
+sys.path.insert(0, THISDIR)  # für uWSGI
 
 app = flask.Flask(__name__)
-rdb = redis.StrictRedis(host='redis')
+rdb = redis.StrictRedis(host = 'redis')
 
 
 @app.route("/")
 def index():
-    
-    return "Hallo Welt {}".format(rdb.incr("my_counter"))
+    my_counter = rdb.incr("my_counter")
+    return "Hallo Welt {}".format(my_counter)
 
 
 def main():
     app.run(host = "0.0.0.0", port = 8080, debug = True)
-    
-    
+
+
 if __name__ == "__main__":
     main()
